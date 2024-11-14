90d618c0f3c07c236d665aa24e8083bcda407556
phuong205 <nguyenthituphuong785@gmail.com>
2024-11-14 11:28:58 +0700

    mess

--- media/baithi/baithi/model/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $server="localhost";
-    $user="root";
-    $pass="";
-    $database="baithi";
-    $conn=mysqli_connect($server,$user,$pass,$database);
-    mysqli_query($conn, 'set names "utf8"');
-?>
